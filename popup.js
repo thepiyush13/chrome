1d843995d5c470442852d54041a73b3e1323496c
piyush <thepiyush13@gmail.com>
2016-03-12 00:04:37 -0600

    switch windows also

--- popup.js
+++ popup.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var tabList = "<ul>";
  chrome.tabs.query({}, function(tabs) { // blah 
  	for(var i=0;i<tabs.length;i++){
- 		tabList+="<li><a id='"+tabs[i].id+"' href='"+tabs[i].url+"'>";
+ 		tabList+="<li><a id='"+tabs[i].windowId+"|"+tabs[i].id+	"' href='"+tabs[i].url+"'>";
  		tabList+="<img src='"+tabs[i].favIconUrl+"' style='width:16px;height:16px;'>";
  		tabList+=tabs[i].title;
  		tabList+="</a></li>";
@@ -24,10 +24,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
 document.addEventListener("click", function(){ 
 
-var id = (this.activeElement.id);
+var cid = (this.activeElement.id);
+var temp = cid.split("|")
+var window = temp[0]
+var id = temp[1]
  chrome.runtime.getBackgroundPage(function(eventPage) {
-       eventPage.switchTab(id);
-    }); 
+       eventPage.switchTab(window,id);
+    });
 
 });
 
--- run.js
+++ run.js
@@ -1,6 +1,7 @@
 // This function is called onload in the popup code
-function switchTab(id) { 
+function switchTab(window,id) { 
 //alert(id)
+chrome.windows.update(parseInt(window), {focused: true})
      chrome.tabs.update(parseInt(id), {active: true});
     
 }; 
